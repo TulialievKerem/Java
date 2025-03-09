0ca9b8431db9cecafbf69bd603d584f41dbd838c
BreakerThings <amogusisnotsus228@gmail.com>
2025-03-09 21:16:37 +0300

    m2

---
+++ m2/Athlet.java
@@ -0,0 +1,5 @@
+package m2;
+
+public interface Athlet {
+  void compete();
+}
---
+++ m2/Runner.java
@@ -0,0 +1,8 @@
+package m2;
+
+public class Runner implements Athlet {
+  @Override
+  public void compete () {
+    System.out.println("Im at a competition");
+  }
+}
